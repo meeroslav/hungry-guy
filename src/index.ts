c1fd2d01c8c69de8aa64e84b6b682d33c65d51f1
Miroslav Jonas <missing.manual@gmail.com>
2019-08-17 20:36:04 +0200

    build: add mini extract css

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World from your main file!");
+import './style.scss';
+
+function init() {
+  const h1 = document.createElement('h1');
+  h1.innerText = 'Some random text';
+  document.body.appendChild(h1);
+
+  const canvas = document.createElement('canvas');
+  canvas.className = 'game-canvas';
+  document.body.appendChild(canvas);
+}
+
+init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,30 @@
 const path = require('path');
 const webpack = require('webpack');
-
-
-
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-
-
-
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- * 
- * https://github.com/jantimon/html-webpack-plugin
- * 
- */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
 
   output: {
-    filename: '[name].[chunkhash].js',
+    filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
 
-  plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
 
   module: {
     rules: [{
@@ -48,9 +32,23 @@ module.exports = {
       loader: 'ts-loader',
       include: [path.resolve(__dirname, 'src')],
       exclude: [/node_modules/]
+    }, {
+      test: /\.scss$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            // you can specify a publicPath here
+            // by default it uses publicPath in webpackOptions.output
+            publicPath: '../',
+            hmr: process.env.NODE_ENV === 'development',
+          },
+        },
+        'css-loader', // translates CSS into CommonJS
+        'sass-loader', // compiles Sass to CSS, using Node Sass by default
+      ],
     }]
   },
-
   optimization: {
     splitChunks: {
       cacheGroups: {
@@ -59,19 +57,17 @@ module.exports = {
           test: /[\\/]node_modules[\\/]/
         }
       },
-
       chunks: 'async',
       minChunks: 1,
       minSize: 30000,
       name: true
     }
   },
-
   devServer: {
-    open: true
+    open: true,
+    hot: true
   },
-
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   }
-}
+};
