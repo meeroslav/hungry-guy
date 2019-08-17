18acc96e02432271b63cb2026fe13777c4d4c8dd
Miroslav Jonas <missing.manual@gmail.com>
2019-08-17 20:42:18 +0200

    build: replace webpack with parcel

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './style.scss';
 
 function init() {
   const h1 = document.createElement('h1');
-  h1.innerText = 'Some random text';
+  h1.innerText = 'Some random text changed';
   document.body.appendChild(h1);
 
   const canvas = document.createElement('canvas');
