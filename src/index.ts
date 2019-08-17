9ea44947c515a985e5947dacd0ae49f0b4a37a9b
Miroslav Jonas <missing.manual@gmail.com>
2019-08-17 21:48:23 +0200

    feat: move canvas to html

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ function init() {
   const h1 = document.createElement('h1');
   h1.innerText = 'Some random text changed';
   document.body.appendChild(h1);
-
-  const canvas = document.createElement('canvas');
-  canvas.className = 'game-canvas';
-  document.body.appendChild(canvas);
 }
 
 init();
