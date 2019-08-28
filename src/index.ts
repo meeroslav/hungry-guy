75ef4964fec7ee7e166b9d714b530ef2dc7bc3e8
Miroslav Jonas <missing.manual@gmail.com>
2019-08-28 21:55:52 +0200

    feat: adapt svg rendering to canvas size

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ function init() {
   document.body.appendChild(h1);
 
   const canvas: HTMLCanvasElement = document.getElementById('game-canvas') as HTMLCanvasElement;
+  const cWidth = canvas.offsetWidth;
+  const cHeight = canvas.offsetHeight;
+  const wRatio = cWidth / 100;
+  const hRatio = cHeight / 100;
   if (canvas) {
     const ctx = canvas.getContext('2d');
     ctx.imageSmoothingEnabled = false;
@@ -13,9 +17,12 @@ function init() {
     const img = new Image();
     img.onload = function () {
       // @ts-ignore
-      ctx.drawImage(img, 20, 20, 50, 50);
+      ctx.drawImage(img, 20 * wRatio, 20 * hRatio, 50 * wRatio, 50 * hRatio);
     };
     img.src = 'https://upload.wikimedia.org/wikipedia/commons/0/02/SVG_logo.svg';
+
+    canvas.setAttribute('height', cWidth);
+    canvas.setAttribute('width', cHeight);
   }
 }
 
