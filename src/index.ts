ee7e8ed8196da647384ad5dfbe4c9ec79da65dee
Miroslav Jonas <missing.manual@gmail.com>
2019-08-28 21:46:05 +0200

    feat: add svg to canvas

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,19 @@ function init() {
   const h1 = document.createElement('h1');
   h1.innerText = 'Some random text changed';
   document.body.appendChild(h1);
+
+  const canvas: HTMLCanvasElement = document.getElementById('game-canvas') as HTMLCanvasElement;
+  if (canvas) {
+    const ctx = canvas.getContext('2d');
+    ctx.imageSmoothingEnabled = false;
+
+    const img = new Image();
+    img.onload = function () {
+      // @ts-ignore
+      ctx.drawImage(img, 20, 20, 50, 50);
+    };
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/0/02/SVG_logo.svg';
+  }
 }
 
 init();
