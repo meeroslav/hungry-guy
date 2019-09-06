362fad6dee81ad341a82832d2a3a76cd78046353
Miroslav Jonas <missing.manual@gmail.com>
2019-09-07 00:01:02 +0200

    feat: add score and lives

--- src/game-state.ts
+++ src/game-state.ts
@@ -3,12 +3,15 @@ import { ALL_FOOD, Food } from './game-food';
 export const MAX_X = 10;
 export const MAX_Y = 10;
 export const GAME_SPEED = 500;
+export const LIVES = 3;
 
 export interface GameState {
   food: Food;
   foodX: number;
   foodY: number;
   chefX: number;
+  score: number;
+  lives: number;
 }
 
 export function generateInitialState(): GameState {
@@ -17,5 +20,7 @@ export function generateInitialState(): GameState {
     food: ALL_FOOD[Math.floor(Math.random() * ALL_FOOD.length)],
     foodY: -1,
     foodX: Math.floor(Math.random() * MAX_X),
+    score: 0,
+    lives: LIVES
   };
 }
