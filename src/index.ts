7ea35cc314a91a11f7405e4c59fc9d0130d2f868
Miroslav Jonas <missing.manual@gmail.com>
2019-09-06 22:46:50 +0200

    feat: add gradient to body

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@ import { initCanvasCtx, renderState } from './game-render';
 import { GameState, generateInitialState } from './game-state';
 
 function init() {
-  const h1 = document.createElement('h1');
-  h1.innerText = 'Some random text changed';
-  document.body.appendChild(h1);
-
   const canvas: HTMLCanvasElement = document.getElementById('game-canvas') as HTMLCanvasElement;
   const ctx = initCanvasCtx(canvas);
 
