0bcb67fbabb8296098de7fd04b56514ebe5e26f7
Miroslav Jonas <missing.manual@gmail.com>
2019-09-06 23:56:53 +0200

    feat: add keyboard logic

--- src/game-actions.ts
+++ src/game-actions.ts
@@ -10,5 +10,11 @@ export function calculateState(state: GameState, action: GameAction): GameState 
   if (action === GameAction.MoveDown) {
     return { ...state, foodY: state.foodY + state.food.speed }
   }
+  if (action === GameAction.ChefLeft) {
+    return { ...state, chefX: state.chefX - 1 }
+  }
+  if (action === GameAction.ChefRight) {
+    return { ...state, chefX: state.chefX + 1 }
+  }
   return state;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import './style.scss';
 import { initCanvasCtx, renderState } from './game-render';
 import { GAME_SPEED, GameState, generateInitialState } from './game-state';
-import { interval } from 'rxjs';
+import { fromEvent, interval, merge } from 'rxjs';
 import { map, scan } from 'rxjs/operators';
 import { calculateState, GameAction } from './game-actions';
+import { filter } from 'rxjs/internal/operators/filter';
+import { tap } from 'rxjs/internal/operators/tap';
 
 function init() {
   const canvas: HTMLCanvasElement = document.getElementById('game-canvas') as HTMLCanvasElement;
@@ -11,10 +13,32 @@ function init() {
 
   const ticker$ = interval(GAME_SPEED)
     .pipe(
-      map(() => GameAction.MoveDown),
+      map(() => GameAction.MoveDown)
+    );
+
+  const keyDown$ = fromEvent(document, 'keydown')
+    .pipe(
+      map((key: KeyboardEvent) => key.code),
+      map(code => {
+        if (code === 'ArrowLeft') {
+          return GameAction.ChefLeft;
+        }
+        if (code === 'ArrowRight') {
+          return GameAction.ChefRight;
+        }
+        if (code === 'ArrowDown') {
+          return GameAction.MoveDown;
+        }
+        return null;
+      }),
+      filter(Boolean)
+    );
+
+  merge(ticker$, keyDown$)
+    .pipe(
       scan(calculateState, generateInitialState())
     )
-    .subscribe(n => renderState(n, ctx));
+    .subscribe(n => renderState(n, ctx))
 }
 
 init();
