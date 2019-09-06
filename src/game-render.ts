5911151cca7e6965780bd3707f4d1f56cbf2c730
Miroslav Jonas <missing.manual@gmail.com>
2019-09-06 22:34:01 +0200

    feat: add render function

--- src/game-render.ts
+++ src/game-render.ts
@@ -1,5 +1,8 @@
+import { GameState } from './game-state';
+import { FOOD_SIZE } from './game-food';
+
 export interface Ctx {
-  ctx: CanvasRenderingContext2D;
+  context: CanvasRenderingContext2D;
   wRatio: number;
   hRatio: number;
 }
@@ -10,10 +13,34 @@ export function initCanvasCtx(canvas: HTMLCanvasElement): Ctx {
   const wRatio = cWidth / 100;
   const hRatio = cHeight / 100;
 
-  const ctx = canvas.getContext('2d');
-  ctx.imageSmoothingEnabled = false;
+  const context = canvas.getContext('2d');
+  context.imageSmoothingEnabled = false;
   canvas.setAttribute('height', cWidth.toString());
   canvas.setAttribute('width', cHeight.toString());
 
-  return { ctx, wRatio, hRatio };
+  return { context, wRatio, hRatio };
+}
+
+export function renderState(state: GameState, ctx: Ctx) {
+  fillBackground(ctx);
+  drawFood(state, ctx);
+}
+
+function fillBackground(ctx: Ctx) {
+  const grd = ctx.context.createLinearGradient(0, 0, 0, 100 * ctx.hRatio);
+  grd.addColorStop(0, '#80c2ff');
+  grd.addColorStop(1, '#436caa');
+  ctx.context.fillStyle = grd;
+  ctx.context.fillRect(0, 0, 100 * ctx.wRatio, 100 * ctx.hRatio);
+}
+
+function drawFood(state: GameState, ctx: Ctx) {
+  const image = new Image();
+  image.onload = () => {
+  //   // @ts-ignore
+  ctx.context.drawImage(image,
+    state.food.foodX * ctx.wRatio,
+    state.food.foodY * ctx.hRatio, FOOD_SIZE * ctx.wRatio, FOOD_SIZE * ctx.hRatio);
+  };
+  image.src = state.food.item.svg;
 }
--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,20 @@ function init() {
   document.body.appendChild(h1);
 
   const canvas: HTMLCanvasElement = document.getElementById('game-canvas') as HTMLCanvasElement;
-  const ctxModel = initCanvasCtx(canvas);
-
-    const grd = ctxModel.ctx.createLinearGradient(0, 0, 0, 100 * ctxModel.hRatio);
-    grd.addColorStop(0, '#80c2ff');
-    grd.addColorStop(1, '#436caa');
-
-// Fill with gradient
-    ctxModel.ctx.fillStyle = grd;
-    ctxModel.ctx.fillRect(0, 0, 100 * ctxModel.wRatio, 100 * ctxModel.hRatio);
+  const ctx = initCanvasCtx(canvas);
 
     const img = new Image();
     img.onload = () => {
       // @ts-ignore
-      ctxModel.ctx.drawImage(img, 20 * ctxModel.wRatio, 20 * ctxModel.hRatio, 50 * ctxModel.wRatio,
-        50 * ctxModel.hRatio);
+      ctx.ctx.drawImage(img, 20 * ctx.wRatio, 20 * ctx.hRatio, 50 * ctx.wRatio,
+        50 * ctx.hRatio);
     };
     img.src = 'https://upload.wikimedia.org/wikipedia/commons/0/02/SVG_logo.svg';
     // const cartmanImg = new Image();
     // cartmanImg.onload = () => {
     //   // @ts-ignore
-    //   ctxModel.ctx.drawImage(cartmanImg, 50 * ctxModel.wRatio, 50 * ctxModel.hRatio, 50 * ctxModel.wRatio,
-    //     50 * ctxModel.hRatio);
+    //   ctx.ctx.drawImage(cartmanImg, 50 * ctx.wRatio, 50 * ctx.hRatio, 50 * ctx.wRatio,
+    //     50 * ctx.hRatio);
     // };
     // cartmanImg.src = cartman;
 }
