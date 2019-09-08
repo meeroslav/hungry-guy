f5c710f6bc7f0b1b54acca59eff71d118294af6a
Miroslav Jonas <missing.manual@gmail.com>
2019-09-08 21:06:04 +0200

    feat: food follows chef and basket if already added

--- src/game-reducer.ts
+++ src/game-reducer.ts
@@ -1,4 +1,4 @@
-import { GameState, MAX_X } from './game-state';
+import { GameState, generateFoodItem, MAX_X, MAX_Y } from './game-state';
 import { ALL_FOOD } from './game-images';
 
 export enum GameAction {
@@ -6,6 +6,8 @@ export enum GameAction {
   ChefLeft = 'ChefLeft',
   ChefRight = 'ChefRight'
 }
+
+const SCORE_MULTIPLIER = 40;
 
 export function keyToGameAction(event: KeyboardEvent): GameAction {
   if (event.code === 'ArrowLeft') {
@@ -28,33 +30,35 @@ export function calculateState(state: GameState, action: GameAction): GameState 
   // OTHERWISE
   if (action === GameAction.MoveDown) {
     const foodY = state.foodY + state.food.speed;
-    if (foodY === 9 && state.foodX === state.chefX) {
-      return {
-        ...state,
-        score: state.score + state.food.speed * 40,
-        food: ALL_FOOD[Math.floor(Math.random() * ALL_FOOD.length)],
-        foodY: -1,
-        foodX: Math.floor(Math.random() * MAX_X)
-      };
-    }
-    if (foodY === 9 && state.foodX !== state.chefX) {
+    if (foodY === MAX_Y - 1) {
+      // INCREASE SCORE
+      if (state.foodX === state.chefX) {
+        return { ...state, foodY, score: state.score + state.food.speed * SCORE_MULTIPLIER };
+      }
+      // LIFE LOST
       return { ...state, foodY, lives: state.lives - 1 };
     }
-    if (foodY === 10) {
-      return {
-        ...state,
-        food: ALL_FOOD[Math.floor(Math.random() * ALL_FOOD.length)],
-        foodY: -1,
-        foodX: Math.floor(Math.random() * MAX_X),
-      }
+    // MAKE NEW FOOD
+    if (foodY === MAX_Y) {
+      return { ...state, ...generateFoodItem() }
     }
     return { ...state, foodY }
   }
   if (action === GameAction.ChefLeft && state.chefX > 0) {
-    return { ...state, chefX: state.chefX - 1 }
+    const chefX = state.chefX - 1;
+    return { ...state, chefX, foodX: calculateFoodX(state, chefX) }
   }
   if (action === GameAction.ChefRight && state.chefX < MAX_X - 1) {
-    return { ...state, chefX: state.chefX + 1 }
+    const chefX = state.chefX + 1;
+    return { ...state, chefX, foodX: calculateFoodX(state, chefX) }
   }
   return state;
+}
+
+// Move food together with Chef, if already added to the basket
+function calculateFoodX(state: GameState, newChefX: number): number {
+  if (state.foodY >= 9 && state.foodX === state.chefX) {
+    return newChefX;
+  }
+  return state.foodX;
 }
--- src/game-state.ts
+++ src/game-state.ts
@@ -5,10 +5,13 @@ export const MAX_Y = 10;
 export const GAME_SPEED = 200;
 export const LIVES = 3;
 
-export interface GameState {
+interface FoodState {
   food: Food;
   foodX: number;
   foodY: number;
+}
+
+export interface GameState extends FoodState {
   chefX: number;
   score: number;
   lives: number;
@@ -17,10 +20,16 @@ export interface GameState {
 export function generateInitialState(): GameState {
   return {
     chefX: 5,
+    score: 0,
+    lives: LIVES,
+    ...generateFoodItem()
+  };
+}
+
+export function generateFoodItem(): FoodState {
+  return {
     food: ALL_FOOD[Math.floor(Math.random() * ALL_FOOD.length)],
     foodY: -1,
     foodX: Math.floor(Math.random() * MAX_X),
-    score: 0,
-    lives: LIVES
   };
 }
